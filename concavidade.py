7961a20a9ff25339bf25117c0914ea731073f4b5
Juliana Yamamoto <31044391+JNamieYamamoto@users.noreply.github.com>
2024-06-11 17:24:05 -0300

    Update concavidade.py

--- concavidade.py
+++ concavidade.py
@@ -1,29 +1,31 @@
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-def EDO(y, x):
+# Define a função da EDO
+def edo(x, y):
     return x - 2*y
 
-y0 = 4  # y(x_0)
-x = np.linspace(-5, 5, 1000)  # Valores de x
+# Define o intervalo de x -> (t_0,T)
+x_span = (-5,5)
 
-# Resultado da EDO
-sol = odeint(EDO, y0, x)
+# Define a condição inicial -> y(t_0)
+y0 = [4]
 
-# Calculando a segunda derivada
-dy_dx = sol[:, 0]  # y(x)
-d2y_dx2 = np.gradient(dy_dx, x)
+# Resolve a EDO
+sol = solve_ivp(edo, x_span, y0, t_eval=np.linspace(x_span[0], x_span[1], 1000))
 
-# Determinando a concavidade
+# Calcula a concavidade
+dy_dx = sol.y[0]
+d2y_dx2 = np.gradient(dy_dx, sol.t)
 concavity = np.sign(d2y_dx2)
 
 # Plot da solução e concavidade
-plt.plot(x, dy_dx, label='Solução Exata', color='black')
-plt.plot(x, concavity, label='Concavidade', color='black', linestyle='dashed')
+plt.plot(sol.t, sol.y[0], color='black', label='Solução da EDO')
+plt.plot(sol.t, concavity, color='black', label='Concavidade', linestyle='--')
 plt.xlabel('x')
 plt.ylabel('y(x) e concavidade')
-plt.title('Solução Exata e Concavidade')
+plt.title('Solução da EDO e Concavidade')
 plt.legend()
 plt.grid(True)
 plt.show()
